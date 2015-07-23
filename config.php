ed6c232551a80fafe3b7816f9e22a4d061eb698b
lekaf <tony.seperoumal@gmail.com>
2015-07-23 12:54:22 +0200

    install ircmaxell/random-lib

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+	//connexion à la base de données
+	define("DBHOST", "localhost"); //ip du serveur MySQL
+    define("DBUSER", "root"); 		//username MySQL
+    define("DBPASS", "");			//mot de passe MySQL
+    define("DBNAME", "taggedtwitter"); 	//nom de la bdd
+
+    //connexion au server smtp (envoi de mail)
+    define("SMTPUSER", "tony.wf3.nanterre@gmail.com");
+    define("SMTPPASS", "nanterre1");
+
+
+?>
