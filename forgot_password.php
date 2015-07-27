57d381da38054a9c2ad7e67864e960d330ed366f
fred-paris <fredem66@gmail.com>
2015-07-27 17:32:52 +0200

    Correct link to page forgot_password_2

--- forgot_password.php
+++ forgot_password.php
@@ -35,58 +35,59 @@
 
 				if ($user){
 				
-				$factory = new RandomLib\Factory;
-				$generator = $factory->getGenerator(new SecurityLib\Strength(SecurityLib\Strength::MEDIUM));
-				$token = $generator->generateString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-');
+					$factory = new RandomLib\Factory;
+					$generator = $factory->getGenerator(new SecurityLib\Strength(SecurityLib\Strength::MEDIUM));
+					$token = $generator->generateString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-');
 
-				$expiry = date("Y-m-d H:i:s", strtotime("+ 1 day"));
+					$expiry = date("Y-m-d H:i:s", strtotime("+ 1 day"));
 
-				$sql = "UPDATE users 
-						SET token = :token,
-						token_expiry = :expiry,
-						date_modified = NOW() 
-						WHERE id = :id";
+					$sql = "UPDATE users 
+							SET token = :token,
+							token_expiry = :expiry,
+							date_modified = NOW() 
+							WHERE id = :id";
 
-				$hashedToken = password_hash($token, PASSWORD_DEFAULT);
+					$hashedToken = password_hash($token, PASSWORD_DEFAULT);
 
-				$sth = $dbh->prepare($sql);
-				$sth->bindValue(":token", $hashedToken);
-				$sth->bindValue(":expiry", $expiry);
-				$sth->bindValue(":id", $user['id']);
+					$sth = $dbh->prepare($sql);
+					$sth->bindValue(":token", $hashedToken);
+					$sth->bindValue(":expiry", $expiry);
+					$sth->bindValue(":id", $user['id']);
 
-				if ($sth->execute()){
+					if ($sth->execute()){
 
-					$resetLink = ROOTURL . "/forgot_password.php?token=$token&email=$email";
+						$resetLink = ROOTURL . "/forgot_password_2.php?token=$token&email=$email";
 
-					$mail = getConfiguredMailer();
+						$mail = getConfiguredMailer();
 
-					//qui envoie, et qui reçoit
-					$mail->setFrom('accounts@wf3-auth.com', 'WF3 Auth');
-					$mail->addAddress('fred.wf3@gmail.com', 'Fred');
-					$mail->addAddress($email, $user['username']);
+						//qui envoie, et qui reçoit
+						$mail->setFrom('accounts@wf3-auth.com', 'Tony');
+						$mail->addAddress('fred.wf3@gmail.com', 'Fred');
+						$mail->addAddress($email, $user['username']);
 
-					$mail->Subject = 'Mot de passe oublié';
+						$mail->Subject = 'Mot de passe oublié';
 
-					$mail->Body = 
-					'<p>Vous avez oublié votre mot de passe taggedTwitter ?<br />
-					<a href="'.$resetLink.'">
-					Cliquez ici pour créer un nouveau mot de passe</a></p>
-					<p>Si vous n\'êtes pas à l\'origine de cette demande, vous pouvez ignorer ce message</p>';
+						$mail->Body = 
+						'<p>Vous avez oublié votre mot de passe taggedTwitter ?<br />
+						<a href="'.$resetLink.'">
+						Cliquez ici pour créer un nouveau mot de passe</a></p>
+						<p>Si vous n\'êtes pas à l\'origine de cette demande, vous pouvez ignorer ce message</p>';
 
-					//pièce jointe
-					//$mail->addAttachment('panda.gif');
+						//pièce jointe
+						//$mail->addAttachment('panda.gif');
 
-					if (!$mail->send()) {
-						$error = "Une erreur de survenue. Le mail n'a pas été envoyé !";
-					} else {
-						$message = "Allez voir vos mails !";
+						if (!$mail->send()) {
+							$error = "Une erreur de survenue. Le mail n'a pas été envoyé !";
+						} else {
+							$message = "Allez voir vos mails !";
+						}
+					}
+					else {
+						$error = "Cet email n'est pas inscrit ici !";
 					}
 				}
-				else {
-				$error = "Cet email n'est pas inscrit ici !";
 			}
 		}
-	}
 ?>
 <!DOCTYPE html>
 <html lang="fr-FR">
@@ -122,4 +123,3 @@
 
 </body>
 </html>
-
