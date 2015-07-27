7696e3e6c1cb578ddf4d5e8d7783686579eed992
lekaf <tony.seperoumal@gmail.com>
2015-07-27 17:19:38 +0200

    affichage tags

--- tags.php
+++ tags.php
@@ -3,16 +3,47 @@
 	include('config.php');
 	include('db_connexion.php');
 	include('vendor/autoload.php');
+	include('fonctions.php');
 
 
-	for($i=0; $i<10; $i++){
-		$faker = Faker\Factory::create();
-		$name = $faker->text($maxNbChars = 100);
+	// for($i=0; $i<1; $i++){
+	// 	$faker = Faker\Factory::create();
+	// 	$name = $faker->text($maxNbChars = 100);
 		
 
-		$sql = "INSERT INTO tag(id, name, date_created, date_modified)
-				VALUES (NULL, :name, NOW(), NOW())";
-		$sth = $dbh->prepare($sql);
-		$sth->bindValue(':name' , $name);
-		$sth->execute();
-	}
+	// 	$sql = "INSERT INTO tag(id, name, date_created, date_modified)
+	// 			VALUES (NULL, :name, NOW(), NOW())";
+	// 	$sth = $dbh->prepare($sql);
+	// 	$sth->bindValue(':name' , $name);
+	// 	$sth->execute();
+	// }
+
+	
+	$sql = "SELECT name 
+			FROM tag";
+	$sth = $dbh->prepare($sql);
+	$sth->execute();
+
+	$tags = $sth->fetch();
+
+	
+	foreach ($tags as $tag) {
+			echo $tag;
+		}
+		
+	
+
+	$sql = "SELECT tweets.messages, tag.name
+			FROM tweets
+			LEFT JOIN tag
+			ON tweets.tag_id = tag.id";
+	$sth = $dbh->prepare($sql);
+	$sth->execute();
+
+
+
+
+
+
+
+	
